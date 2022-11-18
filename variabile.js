55bbc7e28dfbfac81c0b1141bdc6dfe83f5b1d72
Mihail Ninel MARCU <dreamsoft@dsflat.eu>
2022-11-18 12:24:32 +0200

    I add some logics in js true / false statements to play with functions and objects

--- variabile.js
+++ variabile.js
@@ -2,6 +2,7 @@ let favColor = 'blue';
 let myFavColors =['blue', 'red', 'green'];
 let numOffavColors = 3;
 let hasGotFavColors = false;
+
 let richObject = {
     firdtName: 'Rich',
     lastName: 'Amstrong',
@@ -12,4 +13,33 @@ let richObject = {
 
 let whatIsMyFavoriteColor = function(){
 
+}
+
+let afiseazaMesaj = function(){
+
+}
+
+let car = {
+    make: 'VW',
+    type: 'Passat',
+    color: 'gray',
+    isTurnedOn: false,
+    numbersOffWells: 4,
+    seats: [
+            'seat1',
+            'seat2',
+            'seat3',
+            'seat4'
+            ],
+    turnOn: function(){
+        this.isTurnedOn = true;
+    },
+    switchCar: function(isON){
+        console.log('Tha car is on? ' + isON);
+        if(isON == true){
+            this.isTurnedOn=true;
+        }else{
+            this.isTurnedOn=false;
+        }
+    }
 }
