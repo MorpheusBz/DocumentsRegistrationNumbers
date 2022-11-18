6cb59fe4155d0d0978bcc5a81a4d491be49f5465
Mihail Ninel MARCU <dreamsoft@dsflat.eu>
2022-11-18 09:05:04 +0200

    I will add some js file to learn with them

---
+++ variabile.js
@@ -0,0 +1,15 @@
+let favColor = 'blue';
+let myFavColors =['blue', 'red', 'green'];
+let numOffavColors = 3;
+let hasGotFavColors = false;
+let richObject = {
+    firdtName: 'Rich',
+    lastName: 'Amstrong',
+    favColor: ['blue', 'red', 'green'],
+    yearsAlive: 103,
+    isMale: true,
+}
+
+let whatIsMyFavoriteColor = function(){
+
+}
